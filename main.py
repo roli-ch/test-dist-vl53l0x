34ed2deadca0d85804e7a0767c332ab6b7982879
Roli <129250229+roli-ch@users.noreply.github.com>
2026-06-05 13:53:00 +0200

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 Rangefinder.init()
+print("init")
 
 def on_forever():
+    serial.write_line("Distanz")
     serial.write_number(Rangefinder.distance())
     serial.write_string("" + ("\r\n"))
+    pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 Rangefinder.init()
+let pin1 = 10
+console.log("init")
 basic.forever(function on_forever() {
+    serial.writeLine("-------")
+    serial.writeLine("Distanz")
     serial.writeNumber(Rangefinder.distance())
     serial.writeString("" + "\r\n")
+    pin1 = pins.digitalReadPin(DigitalPin.P1)
+    serial.writeValue("pin1", pin1)
+    pause(100)
 })
